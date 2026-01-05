00c52d406a630f3b88fe0fc0ef2f51f347492e65
Natasjalm <nama0006@stud.ek.dk>
2026-01-05 14:42:52 +0100

    næsten done

---
+++ popup.js
@@ -0,0 +1,16 @@
+// Åbn popups
+document.querySelector("#cta_mobile").addEventListener("click", () => {
+  document.querySelector("#popup-mobile").showPopover();
+});
+
+document.querySelector("#cta_paws").addEventListener("click", () => {
+  document.querySelector("#popup-paws").showPopover();
+});
+
+document.querySelector("#cta_grinekaos").addEventListener("click", () => {
+  document.querySelector("#popup-grinekaos").showPopover();
+});
+
+document.querySelector("#cta_magt").addEventListener("click", () => {
+  document.querySelector("#popup-magt").showPopover();
+});
